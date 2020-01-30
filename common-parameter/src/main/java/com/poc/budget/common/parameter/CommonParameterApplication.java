47a99cb1a638f7a61430b08705c48877f38f9791
SR316860 <SR316860@L-156140490.wipro.com>
2020-01-30 15:49:46 +0530

    CommonParameter Service Project

---
+++ common-parameter/src/main/java/com/poc/budget/common/parameter/CommonParameterApplication.java
@@ -0,0 +1,13 @@
+package com.poc.budget.common.parameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonParameterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonParameterApplication.class, args);
+	}
+
+}
---
+++ common-parameter/src/test/java/com/poc/budget/common/parameter/CommonParameterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.budget.common.parameter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonParameterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
