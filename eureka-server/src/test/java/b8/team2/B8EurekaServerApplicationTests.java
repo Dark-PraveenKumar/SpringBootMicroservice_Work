255e03902a8e8933e8386c5681f44a8e3ebcd4dc
SR316860 <SR316860@L-156140490.wipro.com>
2020-01-30 13:12:27 +0530

    Eureka-server

---
+++ eureka-server/src/test/java/b8/team2/B8EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package b8.team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B8EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
